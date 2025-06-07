4bd72940473e42e5811692653a405a82999444e5
sotowang <sotowang@qq.com>
2025-06-07 18:41:23 +0800

    contact us edit

--- app/page.tsx
+++ app/page.tsx
@@ -396,10 +396,7 @@ export default function Home() {
               <h3 className="text-lg font-semibold mb-4">Contact Us</h3>
               <ul className="space-y-2">
                 <li className="text-gray-400">
-                  Email: support@meetinggpt.com
-                </li>
-                <li className="text-gray-400">
-                  Phone: +1 (555) 123-4567
+                  Email: hi@20280101.xyz
                 </li>
               </ul>
             </div>
